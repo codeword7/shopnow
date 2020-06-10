7499d0af7f2fd28895b71e99ac72e122e662b909
Neeraj Yadav <ny29599@gmail.com>
2020-06-10 09:31:39 +0530

    added homepage with styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Homepage from './homepage.component'
 import './App.css';
 
 function App() {
   return (
     <div>
-    Hello world
+      <Homepage />
     </div>
   );
 }
